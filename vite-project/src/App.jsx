992391b02ed038593ea6455531f6b4f548a96b85
studentzzang <studentzzang@gmail.com>
2024-09-23 15:41:39 +0900

    add todo ui

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -28,10 +28,16 @@ export default class App extends Component{
     value: ""
   }
 
-  deleteTodo = (id) => {
-    let todo = this.todoData.filter(data => data.id !== id);
-    console.log(todo);
+  addTodo = () => {
+
   }
+
+  deleteTodo = (id) => {
+
+    // 버튼을 누른 부분만 제외한 모든 투두 데이터를 담음
+    let newTodoData = this.state.todoData.filter(data => data.id !== id);
+    this.setState({todoData : newTodoData});
+  };
   
   render() {
 
@@ -41,18 +47,36 @@ export default class App extends Component{
           <div className="title">
             <h2>할 일 목록</h2>
           </div>
+
+          <form style = {{display : 'flex'}}>
+
+          <input 
+            type="text" 
+            name="value"
+            style = {{flex: '10', padding : '5px'}}
+            placeholder="할 일을 입력하세요."
+            value=""
+          />
+
+          <input
+            type="submit" 
+            value="입력"
+            className="submitBtn" 
+            style = {{flex:1}}  
+          />
+          </form>
         
-        {this.todoData.map((data) => (
+        {this.state.todoData.map((data) => (
           <div style={this.bottomBorder()} key={data.id}>
             
             <input type="checkbox" defaultChecked={data.isDone}></input>
               {data.title}
-            <button className="cancelBox" onClick={this.deleteTodo(data.id)}>X</button>
+            <button className="cancelBox" onClick={() => this.deleteTodo(data.id)}>X</button>
 
           </div>
         ))}
-        </div>
 
+        </div>
       </div>
     );
   }
