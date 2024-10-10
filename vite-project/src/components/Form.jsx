3763537a584c24f6f9b34759a9ba8fbfc9d5d3af
studentzzang <studentzzang@gmail.com>
2024-10-10 14:33:46 +0900

    폴더정리

---
+++ vite-project/src/components/Form.jsx
@@ -0,0 +1,29 @@
+export default function Form(handleSubmit, value, setValue){
+
+    const handleChange = (e) => { //할일 추가 form 변경될 때
+        setValue(e.target.value);
+    };
+
+    return (
+
+        
+        <form style = {{display : 'flex'}} onSubmit={handleSubmit}>
+
+          <input 
+            type="text" 
+            name="value"
+            style = {{flex: '10', padding : '5px'}}
+            placeholder="할 일을 입력하세요."
+            value = {value}
+            onChange={handleChange}
+          />
+
+          <input
+            type="submit" 
+            value="입력"
+            className="submitBtn" 
+            style = {{flex:1}}  
+          />
+        </form>
+    )
+}
