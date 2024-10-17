da31f6b5db861bc06c88bbabf6408ee6c5f75203
studentzzang <studentzzang@gmail.com>
2024-10-17 09:29:03 +0900

    tailwind, dnd (error)

--- vite-project/src/components/List.jsx
+++ vite-project/src/components/List.jsx
@@ -1,3 +1,5 @@
+import {DragDropContext, Draggable} from "react-beautiful-dnd";
+
 export default function List({todoData, setTodoData}){
 
     const handleCompleted = (id) => {
@@ -18,22 +20,28 @@ export default function List({todoData, setTodoData}){
         let newTodoData = todoData.filter(data => data.id !== id);
         setTodoData(newTodoData);
     };
-    return (
-        <div>
-            {todoData.map((data) => (
-                <div key={data.id}>
-                    <div className="flex items-center justifi-between w-full px-4 py-1 my-2
-                    text-slate-200 bg-slate-400 border rounded">
-                        <input 
-                            type="checkbox"
-                            defaultChecked={data.completed}
-                            onChange = {() => handleCompleted(data.id)}
-                        />
-                        {data.title}
-                        <button onClick={() => deleteTodo(data.id)}>X</button>
-                    </div>
-                </div>
-            ))}
+
+
+    return (  
+      <div>
+        {todoData.map((data) => (
+          <div key={data.id}>
+            <div className="flex items-center justify-between w-full px-4 py-1 my-2
+            text-slate-200 bg-slate-400 border rounded">
+              <div className="items-center">
+                <input 
+                  type="checkbox"
+                  defaultChecked={data.completed}
+                  onChange = {() => handleCompleted(data.id)}
+                />
+                <span className={data.completed ? "line-through" : "None"}>{data.title}</span>
+              </div>
+            <div className="items-center">
+              <button className="px-4 py-2 float-right" onClick={() => deleteTodo(data.id)}>X</button>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
